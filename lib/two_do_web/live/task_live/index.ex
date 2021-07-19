07974f07ff150891f0ac62f2aaee053d14ea6eac
Chris Dosé <chris.dose@gmail.com>
2021-07-19 00:30:42 -0700

    Basic keyboard shortcuts for tasks

--- lib/two_do_web/live/task_live/index.ex
+++ lib/two_do_web/live/task_live/index.ex
@@ -15,7 +15,8 @@ defmodule TwoDoWeb.TaskLive.Index do
     {:ok,
      socket
      |> assign(:tasks, Tasks.list_tasks(list))
-     |> assign(:list, list)}
+     |> assign(:list, list)
+     |> assign(:keyboard_selected_task_id, nil)}
   end
 
   @impl true
@@ -50,17 +51,90 @@ defmodule TwoDoWeb.TaskLive.Index do
 
   def handle_event("toggle", %{"task" => task_id}, socket) do
     task = Tasks.get_task!(task_id)
+    socket = toggle_task(task, socket)
 
+    {:noreply, socket}
+  end
+
+  def handle_event("shortcut", %{"key" => key}, socket) do
+    socket = handle_keyboard_shortcut(key, socket.assigns.live_action, socket)
+
+    {:noreply, socket}
+  end
+
+  defp toggle_task(task, socket) do
     case task.state do
       :new -> Tasks.complete_task!(task)
       :done -> Tasks.mark_new!(task)
     end
 
-    {:noreply, assign(socket, :tasks, Tasks.list_tasks(socket.assigns.list))}
+    assign(socket, :tasks, Tasks.list_tasks(socket.assigns.list))
   end
 
   @impl true
   def handle_info(:tasks_updated, socket) do
     {:noreply, assign(socket, :tasks, Tasks.list_tasks(socket.assigns.list))}
+  end
+
+  defp handle_keyboard_shortcut(key, :index, socket) when key in ~w(n N) do
+    push_patch(socket, to: Routes.task_index_path(socket, :new, socket.assigns.list))
+  end
+
+  defp handle_keyboard_shortcut("Backspace", :index, socket) do
+    push_redirect(socket, to: Routes.list_index_path(socket, :index))
+  end
+
+  defp handle_keyboard_shortcut(".", :index, socket) do
+    select_next(socket)
+  end
+
+  defp handle_keyboard_shortcut(",", :index, socket) do
+    select_next(socket, -1)
+  end
+
+  defp handle_keyboard_shortcut(">", :index, socket) do
+    socket
+  end
+
+  defp handle_keyboard_shortcut("<", :index, socket) do
+    socket
+  end
+
+  defp handle_keyboard_shortcut(key, :index, socket) when key in ["x", "X", " "] do
+    if task_id = socket.assigns.keyboard_selected_task_id do
+      task = Tasks.get_task!(task_id)
+      toggle_task(task, socket)
+    else
+      socket
+    end
+  end
+
+  defp handle_keyboard_shortcut(key, :index, socket) when key in ~w(e E Enter) do
+    if task_id = socket.assigns.keyboard_selected_task_id do
+      push_patch(socket, to: Routes.task_index_path(socket, :edit, socket.assigns.list, task_id))
+    else
+      socket
+    end
+  end
+
+  defp handle_keyboard_shortcut(_key, _action, socket) do
+    socket
+  end
+
+  defp select_next(socket, direction \\ 1) do
+    if task_id = socket.assigns.keyboard_selected_task_id do
+      idx = Enum.find_index(socket.assigns.tasks, &(&1.id == task_id))
+      idx = rem(idx + direction, length(socket.assigns.tasks))
+      task = Enum.at(socket.assigns.tasks, idx)
+      assign(socket, :keyboard_selected_task_id, task.id)
+    else
+      id =
+        case socket.assigns.tasks do
+          [] -> nil
+          [task | _] -> task.id
+        end
+
+      assign(socket, :keyboard_selected_task_id, id)
+    end
   end
 end
